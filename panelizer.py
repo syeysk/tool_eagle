651a3b6fcc4dfe5fbada43eda623274887136673
Konstantin Polyakov <shyzik_93@mail.ru>
2018-05-18 14:22:51 +0300

    function "set_board_bound" was added

--- panelizer.py
+++ panelizer.py
@@ -52,7 +52,6 @@ class _Panelizer():
             if obj.tag == 'wire' and obj.get('layer') == '20': continue
             self.move_obj(obj, into)
             plain_to.append(obj)
-
         # signals
 
         signals_to = board_to.findall('./drawing/board/signals')[0]
@@ -63,6 +62,8 @@ class _Panelizer():
             for child in signal:
                 self.move_obj(child, into)
                 if child.tag == 'contactref': child.set('element', child.get('element')+postfix)
+                for vertex in child: # for polygon
+                    self.move_obj(vertex, into)                
             signals_to.append(signal)
            
         # elements
@@ -91,23 +92,28 @@ class _Panelizer():
             libraries_to.append(library)
 
     def _set_board_bound(self, root, coords):
-        for i, wire in enumerate(root.findall("./drawing/board/plain/wire[@layer='20']")):
-            pass
-            '''coords = points[i]
-            wire.set('x1', coords['x1'])
-            wire.set('y1', coords['y1'])
-            wire.set('x2', coords['x2'])
-            wire.set('y2', coords['y2'])'''
+        
+        points = [{'x1':coords['x1'], 'y1':coords['y1'], 'x2':coords['x1'], 'y2':coords['y2']},
+                  {'x1':coords['x1'], 'y1':coords['y2'], 'x2':coords['x2'], 'y2':coords['y2']},
+                  {'x1':coords['x2'], 'y1':coords['y2'], 'x2':coords['x2'], 'y2':coords['y1']},
+                  {'x1':coords['x1'], 'y1':coords['y1'], 'x2':coords['x2'], 'y2':coords['y1']}]
 
-    def _get_fact_board_bound(self, root):
+        for i, wire in enumerate(root.findall("./drawing/board/plain/wire[@layer='20']")):
+            coords = points[i]
+            wire.set('x1', str(coords['x1']))
+            wire.set('y1', str(coords['y1']))
+            wire.set('x2', str(coords['x2']))
+            wire.set('y2', str(coords['y2']))
+
+    '''def _get_fact_board_bound(self, root):
         x = []
         y = []
-        for wire in root:
-            if child.tag == 'libraries':continue
-            for name in ['x1', 'x2']: x.append(float(wire.get(name)))
-            for name in ['y1', 'y2']: y.append(float(wire.get(name)))
+        for obj in root:
+            if obj.tag == 'libraries':continue
+            for name in ['x1', 'x2']: x.append(float(obj.get(name)))
+            for name in ['y1', 'y2']: y.append(float(obj.get(name)))
         return { 'x1': min(x), 'y1': min(y), 'x2': max(x), 'y2': max(y) }
-
+    '''
 
     def _get_board_bound(self, root):
         x = []
@@ -117,8 +123,14 @@ class _Panelizer():
             for name in ['y1', 'y2']: y.append(float(wire.get(name)))
         return { 'x1': min(x), 'y1': min(y), 'x2': max(x), 'y2': max(y) }
          
-    def _get_max_names(self, root):
-        elements = [];
+    '''def _get_max_names(self, root):
+        elements = [];'''
+        
+    def _rename_all(self, root):
+        map = {}
+        for element in root.findall("./drawing/board/elements/*"):
+            name = element.get('name')
+            
 
 class Panelizer(_Panelizer):
 
@@ -155,6 +167,9 @@ class Panelizer(_Panelizer):
         fname_out = self.docs[fkey]['fname'] if fname_out is None else fname_out
         self.docs[fkey]['tree'].write(fname_out)
 
+    def rename_all(self, fkey):
+        self._rename_all(self.docs[fkey]['root'])
+
 pan = Panelizer({'tr': '../Termo-Relay/Termo-Relay.brd',
                  'wfr-bt139':'../WIFI-Relay-bt139/main.brd',
                  'wfr':'../WIFI-Relay_v0.2/WIFI-Relay_v0.2.brd'})
@@ -167,11 +182,13 @@ tr_c = pan.get_board_bound('tr')
 wfr_bt_c = pan.get_board_bound('wfr-bt139')
 
 #pan.move('tr', {'x': 10, 'y': 50})
-pan.join('wfr', 'tr', {'x':tr_c['x2']+5, 'y':tr_c['y1']})
-pan.join('tr', 'wfr-bt139', {'x':wfr_bt_c['x1'], 'y':wfr_bt_c['y2']+5})
+pan.join('wfr', 'tr', {'x':tr_c['x2']+2, 'y':tr_c['y1']})
+pan.join('tr', 'wfr-bt139', {'x':wfr_bt_c['x1'], 'y':wfr_bt_c['y2']+1})
 
-wfr_bt_c = pan.get_fact_board_bound('wfr-bt139')
-pan.set_board_bound('wfr-bt139', wfr_bt_c)
+#wfr_bt_c = pan.get_fact_board_bound('wfr-bt139')
+pan.set_board_bound('wfr-bt139', {'x1':0, 'y1':0, 'x2':100, 'y2':100})#wfr_bt_c)
+
+pan.rename_all('wfr-bt139')
 
 pan.write('wfr-bt139', '../Termo-Relay2.brd')
 
